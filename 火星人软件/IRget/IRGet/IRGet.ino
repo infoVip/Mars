0dfe8ac8139c0fd038858259dd840acd0f8426a8
oa25Diy <baomin@oa25.org>
2017-01-05 23:38:33 +0800

    20170105Z
    20170105Z

--- 火星人软件/IRget/IRGet/IRGet.ino
+++ 火星人软件/IRget/IRGet/IRGet.ino
@@ -1,4 +1,11 @@
 /*
+ *本程序 为Mars(火星人) 验证红外指令的程序 
+ *      本程序的功能是 将 EEPROM中的红外码 读入内存
+ *      
+ *  http://www.oa25.org
+ *  
+ *  http://blog.oa25.org/?cat=15
+ * 
  * IRremote: IRrecvDemo - demonstrates receiving IR codes with IRrecv
  * An IR detector/demodulator must be connected to the input RECV_PIN.
  * Version 0.1 July, 2009
@@ -22,6 +29,8 @@ decode_results results;
 long testL;
 long La[21] = {-2139121561L,-2139113401L,-2139153691L,-2139137371L,-2139121051L,2155839615,2155823295,2155855935,2155815135,2155847775,2155831455,2155831455,2155864095,2155811055,2155843695,2155827375,2155806975,2155862055,2155827375,2155806975,2155862055};
 //long La[21] = {1086298215,1086306375,1086282405,1086266085,1086298725,1086292095,1086275775,1086308415,1086267615,1086300255,1086283935,1086316575,1086263535,1086296175,1086279855,1086259455,1086314535,1086268125,1086300765,1086284445,1086276285};
+char* LaStr[]={"UP","Dow","LeiL","LeiR","Stop","G01","G02","G03","G04","G05","G06","G07","G08","G09","G0*","G00","G0#","EyeLoNoFF","EyeSing","Sing","EyeRoNoFF"};
+
 //up,dow,Left,R,stop
 // Dumps out the decode_results structure.
 // Call this after IRrecv::decode()
@@ -43,62 +52,13 @@ void dump(decode_results *results) {
     Serial.println("<>");
   }
   
-  if (La[0] == results->value ) Serial.println("<--UP-->");
-  if (La[1] == results->value ) Serial.println("<--Dow-->");
-  if (La[2] == results->value ) Serial.println("<--LeiL-->");
-  if (La[3] == results->value ) Serial.println("<--LeiR-->");
-  if (La[4] == results->value ) Serial.println("<--Stop-->");
-  if (La[5] == results->value ) Serial.println("<--G01-->");
-  if (La[6] == results->value ) Serial.println("<--G02-->");
-  if (La[7] == results->value ) Serial.println("<--G03-->");
-  if (La[8] == results->value ) Serial.println("<--G04-->");
-  if (La[9] == results->value ) Serial.println("<--G05-->");  
-  if (La[10] == results->value ) Serial.println("<--G06-->");
-  if (La[11] == results->value ) Serial.println("<--G07-->");
-  if (La[12] == results->value ) Serial.println("<--G08-->");
-  if (La[13] == results->value ) Serial.println("<--G09-->");
-  if (La[14] == results->value ) Serial.println("<--G0*-->");
-  if (La[15] == results->value ) Serial.println("<--G00-->");
-  if (La[16] == results->value ) Serial.println("<--G0#-->");
-  if (La[17] == results->value ) Serial.println("<--EyeLoNoFF-->");
-  if (La[18] == results->value ) Serial.println("<--EyeSing-->");
-  if (La[19] == results->value ) Serial.println("<--Sing-->");
-  if (La[20] == results->value ) Serial.println("<--EyeRoNoFF-->");
-  
-/*  if (results->decode_type == UNKNOWN) {
-    Serial.println("Could not decode message");
-  } 
-  else {
-    if (results->decode_type == NEC) {
-      Serial.print("Decoded NEC: ");
-    } 
-    else if (results->decode_type == SONY) {
-      Serial.print("Decoded SONY: ");
-    } 
-    else if (results->decode_type == RC5) {
-      Serial.print("Decoded RC5: ");
-    } 
-    else if (results->decode_type == RC6) {
-      Serial.print("Decoded RC6: ");
+  for (int i = 0; i < 21; i++){
+    if (La[i] == results->value ) {
+      Serial.print("<--");
+      Serial.print(LaStr[i]);
+      Serial.println("-->");
     }
-    Serial.print(results->value, HEX);
-    Serial.print(" (");
-    Serial.print(results->bits, DEC);
-    Serial.println(" bits)");
   }
-  Serial.print("Raw (");
-  Serial.print(count, DEC);
-  Serial.print("): ");
-
-  for (int i = 0; i < count; i++) {
-    if ((i % 2) == 1) {
-      Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
-    } 
-    else {
-      Serial.print(-(int)results->rawbuf[i]*USECPERTICK, DEC);
-    }
-    Serial.print(" ");
-  }   */
   Serial.println("");
 }
 
--- 火星人软件/安装标定/Mars_setup/Mars_setup.ino
+++ 火星人软件/安装标定/Mars_setup/Mars_setup.ino
@@ -3,6 +3,7 @@
 //-- 本程序为oa25 Mars 设置和整定程序
 //-- 在GPL许可下发布
 //-- http://www.oa25.org
+//-- http://blog.oa25.org/?cat=15
 //-- Mars Dance smooth criminal
 //-- Released under a GPL licencse
 //-- Originally made for Zowi and Otto project remake for Otto
@@ -40,6 +41,8 @@ Oscillator servo[N_SERVOS];
 int servoDate[N_SERVOS];
 
 
+
+
 //设置舵机的中间位置  90度
 
 
@@ -49,6 +52,18 @@ int inByte =0;
 char inChar= 'A';
 int n = 0;
 int n1 =0;
+
+//红外参数设置
+int RECV_PIN = 12;        // 红外接收管脚 
+int eeAddress = 0;            //EEPROM 的当前地址
+int irstart_addr = 48;   // 红外控制码 EEPROM 的起始地址
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+long testL;
+//long La[21] = {-2139121561L,-2139113401L,-2139153691L,-2139137371L,-2139121051L,2155839615,2155823295,2155855935,2155815135,2155847775,2155831455,2155831455,2155864095,2155811055,2155843695,2155827375,2155806975,2155862055,2155827375,2155806975,2155862055};
+long La[21] = {1086298215,1086306375,1086282405,1086266085,1086298725,1086292095,1086275775,1086308415,1086267615,1086300255,1086283935,1086316575,1086263535,1086296175,1086279855,1086259455,1086314535,1086268125,1086300765,1086284445,1086276285};
+char* LaStr[]={"UP","Dow","LeiL","LeiR","Stop","G01","G02","G03","G04","G05","G06","G07","G08","G09","G0*","G00","G0#","EyeLoNoFF","EyeSing","Sing","EyeRoNoFF"};
 
 
 void setup() {
@@ -62,10 +77,15 @@ void setup() {
   servo[3].attach(PIN_YL);
 
   for(int i=0;i<4;i++) {
-    runServo(i , 90);
+    servoDate[i] = 90;
+    runServo(i , servoDate[i]);
   }
-  
+  irrecv.enableIRIn(); // Start the receiver
+  getIReeprom();
 }
+
+int on = 0;
+unsigned long last = millis();
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -87,9 +107,12 @@ void loop() {
         case 'S':  //舵机命令  
           cmdServo(comDataSub);
           break;
-//        case 'I':
-//          cmdIR(comDataSub);
-//         break;  
+        case 'I':  //红外模块设定
+          cmdIR(comDataSub);
+          break;  
+        case 'V':   //显示
+          cmdShow(comDataSub);
+          break;           
         default: 
           inByte = inByte + 1;
      }
@@ -98,8 +121,104 @@ void loop() {
   }
 }
 
-//=====命令函数=======
+// 获得EEPROM 内存的IR 参数
+void getIReeprom(){
+  int eeAddress = 0;
+  irstart_addr;
+  for(int i = 0 ; i <= 21 ;i++ ){
+    eeAddress = irstart_addr+i*sizeof(long);  
+    EEPROM.get(eeAddress,La[i]);  
+    Serial.println("EEPROM.get-----");
+  }
+}
 
+// 保存IR 参数到EEPROM内存中
+void saveIReeprom(){
+  int eeAddress = 0;
+  irstart_addr;
+  for(int i = 0 ; i <= 21 ;i++ ){
+    eeAddress = irstart_addr+i*sizeof(long);  
+    EEPROM.put(eeAddress,La[i]);  
+    Serial.println("EEPROM.put-----");
+  }
+}
+//=====命令函数=======
+//红外设定
+int cmdIR(String comData){
+  Serial.println(comData);
+  int cLne = comData.length();
+  
+}
+
+void dump(decode_results *results) {
+  int count = results->rawlen;
+  testL = long(results->value);
+  Serial.println(results->value, HEX);
+  Serial.println(results->value);
+  Serial.println("-------Test long ");
+  Serial.println(testL, HEX);
+  Serial.println(testL);
+  if (testL == results->value ){
+    Serial.println("==");
+  } else {
+    Serial.println("<>");
+  }
+
+  for (int i = 0; i < 21; i++){
+    if (La[i] == results->value ) {
+      Serial.print("<--");
+      Serial.print(LaStr[i]);
+      Serial.println("-->");
+    }
+  }
+  Serial.println(""); 
+}  
+
+//------------------------------------------------------------------------------------
+
+//显示
+int cmdShow(String comData){
+  Serial.println(comData);
+  int cLne = comData.length();
+  if (cLne == 1){
+    switch (comData[0]) {  
+       case 'A':
+ //         showAll();  
+          break;
+       case 'S':
+          showServo();
+          break;
+       case 'I':
+          showIr();
+          break; 
+    }
+  }
+}
+
+void showIr(){
+   Serial.println("Show Ir Data ----");
+   for(int i=0;i<21;i++){
+      Serial.print("<--");
+      Serial.print(La[i]);
+      Serial.print("--");
+      Serial.print(LaStr[i]);
+      Serial.println("-->");
+   }
+   Serial.println("Show Ir Data  End ====");   
+}
+
+
+void showServo(){
+   Serial.println("Show Servo Data ----");
+   for(int i=0;i<4;i++){
+      Serial.print("Servo(");
+      Serial.print(i+2);
+      Serial.print(") ==> ");
+      Serial.println(servoDate[i]);
+   }
+   Serial.println("Show Servo Data  End ====");
+}
+//--------------------------------------------------------------------------------
 
 //舵机命令
 int cmdServo(String comData){
